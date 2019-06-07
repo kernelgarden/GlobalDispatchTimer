48af57d1b6aa05f6bc97ed418bb413de68072c43
Kernelgarden <kernelgarden@gmail.com>
2019-06-07 09:35:46 +0900

    add Demo scene

---
+++ Assets/Scripts/Demo/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Global.Timer;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private GlobalDispatchTimer gdt;
+    
+    private void Start()
+    {
+        gdt = GlobalDispatchTimer.Instance;
+    }
+}
